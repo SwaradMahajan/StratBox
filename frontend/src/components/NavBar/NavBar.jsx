880dc0a49d03643538413619e4a3ad69f573b34b
SwaradMahajan <swaradmahajan2006@gmail.com>
2026-07-04 17:32:35 +0530

    Navbar heading to redirect to homepage

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,13 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
     return (
         <nav className="navbar">
 
-            <div className="logo">
+            <Link to="/" className="logo">
                 Strat<span>Box</span>
-            </div>
+            </Link>
 
             <div className="nav-links">
 
